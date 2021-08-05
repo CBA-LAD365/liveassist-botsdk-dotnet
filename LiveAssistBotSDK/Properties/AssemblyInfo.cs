2b59b1193223c974fd062e537346370430110c6e
rob-doyle-cba <69197485+rob-doyle-cba@users.noreply.github.com>
2021-08-05 11:26:20 +0100

    Version updated to 1.0.3

--- LiveAssistBotSDK/Properties/AssemblyInfo.cs
+++ LiveAssistBotSDK/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LiveAssistBotSDK")]
 [assembly: AssemblyDescription("Live Assist Bot SDK")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cafex")]
+[assembly: AssemblyCompany("CBA Japan")]
 [assembly: AssemblyProduct("LiveAssistBotSDK")]
 [assembly: AssemblyCopyright("Copyright © Communication Business Avenue, Inc. 2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
